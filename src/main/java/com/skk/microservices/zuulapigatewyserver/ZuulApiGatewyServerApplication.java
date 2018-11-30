0dc346ca19fca841516e758778b16a2e5e2c5d93
Kumar Kushwaha <s.a.kumar.kushwaha@accenture.com>
2018-11-30 14:26:29 +0530

    Distributed tracing log using amqp,zipkin,sleuth

--- src/main/java/com/skk/microservices/zuulapigatewyserver/ZuulApiGatewyServerApplication.java
+++ src/main/java/com/skk/microservices/zuulapigatewyserver/ZuulApiGatewyServerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,5 +14,11 @@ public class ZuulApiGatewyServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApiGatewyServerApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaultSapler() {
+		
+		return Sampler.ALWAYS_SAMPLE;
 	}
 }
